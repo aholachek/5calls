563263fba5301b46c09e7472c4f69fed592404c3
Alex Holachek <aholachek@gmail.com>
2019-02-24 10:50:33 -0500

    preload state

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -14,10 +14,19 @@ const middlewares: Middleware[] = [thunk];
 export let persistor = {} as Persistor;
 export let store = {} as Store<ApplicationState>;
 
+// @ts-ignore
+const preloadedState = window.__PRELOADED_STATE__;
+// @ts-ignore
+delete window.__PRELOADED_STATE__;
+// @ts-ignore
+window.snapSaveState = () => ({
+  __PRELOADED_STATE__: store.getState()
+});
+
 export default initialState => {
   store = createStore(
     rootReducer,
-    DefaultApplicationState,
+    preloadedState || DefaultApplicationState,
     compose(
       applyMiddleware(...middlewares),
       // This added for Redux Dev Tools - install Chrome or Firefox extension to use
